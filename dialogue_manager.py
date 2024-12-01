374454c02d0de8e5c7098925c9d9f6b1ccfc13bc
rogergheser <w100roger@gmail.com>
2024-12-01 17:54:04 +0100

    query model with history of meaning representation

--- dialogue_manager.py
+++ dialogue_manager.py
@@ -27,7 +27,7 @@ class DialogueManager():
         meaning_representation = self.get_meaning_representation(input_prompt)
         self.state_tracker.update(meaning_representation)
         
-        self.query_dialogue_manager(meaning_representation)
+        self.query_dialogue_manager(self.state_tracker)
 
     def get_meaning_representation(self, input_prompt):
         raw_meaning_rep = self.query_model(self.nlu_cfg['model_name'], self.nlu_cfg['system_prompt_file'], input_prompt)
@@ -50,17 +50,19 @@ class DialogueManager():
 
         meaning_representation = self.get_meaning_representation(input_prompt)
         self.state_tracker.update(meaning_representation)
+        logger.info(self.state_tracker, extra={"color": "green"})
 
-        self.query_dialogue_manager(meaning_representation)
+        self.query_dialogue_manager(self.state_tracker)
 
-    def query_dialogue_manager(self, meaning_representation):
+    def query_dialogue_manager(self, state_tracker: DialogueST):
         """
         Query the dialogue manager model to get the next best action to perform.
         """
+        meaning_representation = state_tracker.to_dict()
         raw_action = self.query_model(self.dm_cfg['model_name'], self.dm_cfg['system_prompt_file'], str(meaning_representation))
         # action = parse_json(raw_action) # TODO what to parse? Do we need to parse?
         # Define a format for asking info
-        logging.debug(raw_action)
+        logger.debug(raw_action)
         try:
             action, argument = extract_action_and_argument(raw_action)
         except:
@@ -71,16 +73,20 @@ class DialogueManager():
         if 'info' in action:
             self.ask_info(argument)
         elif 'confirm' in action:
-            self.confirm_order()
+            self.confirm_order(self.state_tracker.order) 
 
     def confirm_order(self, order):
+        # print in blue
+        print('\033[94m' + "Confirming order")
         lexicalised_ans = self.lexicalise('confirm_order\n' + order)
         self.history.add(lexicalised_ans, 'assistant', 'confirm_order')
         logging.debug(lexicalised_ans)
 
+
     def lexicalise(self, action):
+        logger.debug('lexicalise: ' + action)
         lexicalised_text = self.query_model(self.nlg_cfg['model_name'], self.nlg_cfg['system_prompt_file'], action)
-        
+
         return lexicalised_text
     
     def query_model(self, model_name, system, input_text, max_seq_len=128):
@@ -91,8 +97,15 @@ class DialogueManager():
             pass
         elif user_env == 'amirgheser':
             # we are on the local machine
-            messages = [{'role':'system', 'content': system_prompt}] + self.history.to_msg_history()
-
+            messages = [{
+                            'role': 'system',
+                            'content' : self.history.action_history_str()
+                            },
+                        {
+                            'role':'system',
+                            'content': system_prompt
+                            }]
+            logger.debug(messages, extra={"color": "blue"})
             response = ollama.chat(model=model_name, messages=
                 messages
             )
@@ -118,8 +131,8 @@ nlg_cfg = {
 
 
 if __name__ == '__main__':
+    logger_cfg(logger)
     dm = DialogueManager(nlu_cfg, dm_cfg, nlg_cfg)
     dm.start_conversation()
-    logger_cfg(logger)
 
     print(dm.history.to_msg_history())
--- utils.py
+++ utils.py
@@ -16,6 +16,13 @@ class DialogueST(ABC):
     def is_order(self):
         pass
 
+    @abstractmethod
+    def to_dict(self):
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        pass
 
 class BurgerST(DialogueST):
     def __init__(self):
@@ -30,20 +37,47 @@ class BurgerST(DialogueST):
             'cooking'
             ]
         self.order = {field : None for field in fields}
+        self.sentiment = None
 
     def update(self, parsed_input: dict):
-        for field in self.order:
-            if field in parsed_input:
-                if parsed_input[field] == 'null':
-                    self.order[field] = None
-                else:
-                    self.order[field] = parsed_input[field]
+        """
+        Updates the dialogue state for the slots from the parsed input to the order state tracker.
+        :param parsed_input: dict, parsed slots dictionary from the user.
+        """
+        if 'intent' in parsed_input:
+            if parsed_input['intent'] != 'burger_ordering':
+                logging.warning('Intent is not burger_ordering.')
+                return
+
+        if 'sentiment' in parsed_input:
+            self.sentiment = parsed_input['sentiment']
+        
+        if 'slots' not in parsed_input:
+            logging.warning('No slots found in parsed input.')
+            return
+        parsed_input = parsed_input['slots']
+        for field in parsed_input:
+            if parsed_input[field] == 'null':
+                continue
+            if field in self.order:
+                self.order[field] = parsed_input[field]
+            else:
+                logging.warning(f'Field {field} not found in order fields.')
+
+    def __str__(self):
+        return ', '.join([f'{key}: {value}' for key, value in self.order.items()])
+
+    def to_dict(self):
+        return {"intent": "burger_ordering",
+                "slots" : self.order,
+                "sentiment" : self.sentiment}
 
     def is_order(self):
         for field in self.order:
             if self.order[field] is None:
                 return False
         return True
+    
 
 
 class ConversationHistory():
@@ -64,6 +98,9 @@ class ConversationHistory():
     def get_history(self):
         return self.msg_list
     
+    def action_history_str(self):
+        return ', \n'.join(self.actions)
+
     def to_msg_history(self)->list[dict]:
         return [{'role': role, 'content': msg} for role, msg in zip(self.roles, self.msg_list)]
 
@@ -112,34 +149,61 @@ def parse_json(json_str: str) -> Union[dict, list, None]:
     except TypeError as e:
         raise ValueError(f"Input must be a string: {e}") from e
 
+class ColorFormatter(logging.Formatter):
+    COLORS = {
+        "white": "\033[97m",
+        "red": "\033[91m",
+        "green": "\033[92m",
+        "blue": "\033[94m",
+        "reset": "\033[0m"
+    }
 
-def logger_cfg(logger):
-    # Create handlers
-    stdout_handler = logging.StreamHandler(sys.stdout)
-    stderr_handler = logging.StreamHandler(sys.stderr)
-    file_handler = logging.FileHandler('dialogue_manager.log')
+    def __init__(self, fmt, default_color="white"):
+        super().__init__(fmt)
+        self.default_color = self.COLORS.get(default_color.lower(), self.COLORS["white"])
 
-    # Set levels for handlers
-    stdout_handler.setLevel(logging.INFO)
-    stderr_handler.setLevel(logging.DEBUG)
-    file_handler.setLevel(logging.DEBUG)
+    def format(self, record):
+        # Use the color passed in `record.color` or fall back to the default color
+        color = self.COLORS.get(getattr(record, 'color', '').lower(), self.default_color)
+        msg = super().format(record)
+        return f"{color}{msg}{self.COLORS['reset']}"
 
-    # Create formatters and add them to handlers
-    stdout_formatter = logging.Formatter('%(asctime)s - %(message)s')
-    stderr_formatter = logging.Formatter('\033[32m%(asctime)s - %(levelname)s - %(message)s\033[0m')
-    file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
-    stdout_handler.setFormatter(stdout_formatter)
-    stderr_handler.setFormatter(stderr_formatter)
-    file_handler.setFormatter(file_formatter)
+def logger_cfg(logger, debug_color="red", info_color="white"):
+    logger.setLevel(logging.DEBUG)
 
-    # Add handlers to the logger
-    logger.addHandler(stdout_handler)
-    logger.addHandler(stderr_handler)
-    logger.addHandler(file_handler)
+    # File handler for debug logs
+    debug_file_handler = logging.FileHandler('debug.log')
+    debug_file_handler.setLevel(logging.DEBUG)
+    debug_file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    debug_file_handler.setFormatter(debug_file_formatter)
+
+    # File handler for info logs
+    info_file_handler = logging.FileHandler('info.log')
+    info_file_handler.setLevel(logging.INFO)
+    info_file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    info_file_handler.setFormatter(info_file_formatter)
+
+    # Console handler for debug logs (colorized)
+    console_handler_debug = logging.StreamHandler()
+    console_handler_debug.setLevel(logging.DEBUG)
+    console_handler_debug.setFormatter(ColorFormatter('%(levelname)s - %(message)s', default_color=debug_color))
+
+    # Console handler for info logs (colorized)
+    console_handler_info = logging.StreamHandler()
+    console_handler_info.setLevel(logging.INFO)
+    console_handler_info.setFormatter(ColorFormatter('%(levelname)s - %(message)s', default_color=info_color))
+
+    # Add handlers to logger
+    logger.addHandler(debug_file_handler)
+    logger.addHandler(info_file_handler)
+    logger.addHandler(console_handler_debug)
+    logger.addHandler(console_handler_info)
 
 def extract_action_and_argument(input_string):
     ## TODO add check in case there is more in the output string from the LLM
+    # Remove any ' characters from the input string
+    input_string = input_string.replace("'", "")
     # Define the regex pattern for extracting action and argument
     pattern = r'(\w+)\((\w+)\)'
     match = re.match(pattern, input_string)
